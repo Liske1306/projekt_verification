4a79331a2a1395e98d714cf158309faefad57032
Liske1306 <gregi1306@gmail.com>
2025-10-19 16:56:14 +0200

    initial state after lab1

--- lab01/run/INCA_libs/run.lnx8664.24.09.d/files.ts
+++ lab01/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -1,2 +1,2 @@
 1760390219 /home/student/glis/Downloads/projekt_verification/common/simple_uart_switch.sv
-1760664290 /home/student/glis/Downloads/projekt_verification/lab01/tb/simple_uart_switch_tb.sv
+1760697739 /home/student/glis/Downloads/projekt_verification/lab01/tb/simple_uart_switch_tb.sv
